e5627f9686a22b9e4ef6acf5fcdd929e2cbb69ed
timscheuermann <t.scheuermann2000@gmail.com>
2021-02-14 21:56:37 +0100

    Projektdateien hinzufügen.

---
+++ OpenGLTutorial/main.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#define SDL_MAIN_HANDLED
+
+#ifdef _WIN32
+#include <SDL.h>
+#pragma comment(lib, "SDL2.lib") //Linker Additional Dependencies
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "opengl32.lib")
+#else
+#include <SDL2/SDL.h>
+#endif
+
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float float32;
+typedef double float64;
+
+int main(int argc, char** argv) {
+	SDL_Window* window;
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	window = SDL_CreateWindow("C++ OpenGL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+		std::cin.get();
+		return -1;
+	}
+	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+
+	bool close = false;
+	while (!close) {
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+
+		glEnd();
+
+		SDL_GL_SwapWindow(window);
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				close = true;
+			}
+		}
+
+	}
+
+	return 0;
+}
