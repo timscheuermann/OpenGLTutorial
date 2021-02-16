7f9ebc33bc067d406ff9f72b205c91b80caca8f6
timscheuermann <t.scheuermann2000@gmail.com>
2021-02-16 23:55:34 +0100

    Fixed LNK error

--- OpenGLTutorial/shader.cpp
+++ OpenGLTutorial/shader.cpp
@@ -1,10 +1,11 @@
-#include "shader.h"
+ï»¿#include "shader.h"
 #include <fstream>
 #include <iostream>
 
 Shader::Shader(const char* vertexShaderFilename, const char* fragmentShaderFilename) {
 	shaderId = createShader(vertexShaderFilename, fragmentShaderFilename);
 }
+
 Shader::~Shader() {
 	glDeleteProgram(shaderId);
 }
@@ -12,11 +13,13 @@ Shader::~Shader() {
 void Shader::bind() {
 	glUseProgram(shaderId);
 }
+
 void Shader::unbind() {
 	glUseProgram(0);
 }
 
-GLuint compile(std::string shaderSource, GLenum type) {
+
+GLuint Shader::compile(std::string shaderSource, GLenum type) {
 	GLuint id = glCreateShader(type);
 	const char* src = shaderSource.c_str();
 	glShaderSource(id, 1, &src, 0);
@@ -35,20 +38,22 @@ GLuint compile(std::string shaderSource, GLenum type) {
 	}
 	return id;
 }
-std::string parse(const char* filename) {
+
+std::string Shader::parse(const char* filename) {
 	FILE* file;
 #ifdef _WIN32
-	if(fopen_s(&file, filename, "rb")!=0){
-		std::cout << "File " << filename << "not found" << std::endl;
+	if (fopen_s(&file, filename, "rb") != 0) {
+		std::cout << "File " << filename << " not found" << std::endl;
 		return "";
 	}
 #else
 	file = fopen(filename, "rb");
 	if (file == nullptr) {
-		std::cout << "File " << filename << "not found" << std::endl;
+		std::cout << "File " << filename << " not found" << std::endl;
 		return "";
 	}
 #endif
+
 	std::string contents;
 	fseek(file, 0, SEEK_END);
 	size_t filesize = ftell(file);
@@ -57,19 +62,21 @@ std::string parse(const char* filename) {
 
 	fread(&contents[0], 1, filesize, file);
 	fclose(file);
+
 	return contents;
 }
-GLuint createShader(const char* vertexShaderFilename, const char* fragmentShaderFilename) {
+
+GLuint Shader::createShader(const char* vertexShaderFilename, const char* fragmentShaderFilename) {
 	std::string vertexShaderSource = parse(vertexShaderFilename);
 	std::string fragmentShaderSource = parse(fragmentShaderFilename);
 
 	GLuint program = glCreateProgram();
-	GLuint vs = compile(vertexShaderSource,GL_VERTEX_SHADER);
+	GLuint vs = compile(vertexShaderSource, GL_VERTEX_SHADER);
 	GLuint fs = compile(fragmentShaderSource, GL_FRAGMENT_SHADER);
 
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
-	glLinkProgram(program); // nach dem linken können die Shader wieder detached werden; sogar gelöscht werden
+	glLinkProgram(program);
 
 #ifdef _RELEASE
 	glDetachShader(program, vs);
@@ -78,5 +85,6 @@ GLuint createShader(const char* vertexShaderFilename, const char* fragmentShader
 	glDeleteShader(vs);
 	glDeleteShader(fs);
 #endif
+
 	return program;
 }
--- OpenGLTutorial/shaders/basic.frag
+++ OpenGLTutorial/shaders/basic.frag
@@ -1,6 +1,6 @@
 #version 330 core
 
-layout(location = ) out vec4 color;
+layout(location = 0) out vec4 color;
 
 void main()
 {
